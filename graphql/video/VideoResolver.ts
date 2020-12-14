df5abb3e5cad84b6aea68eadbde30f6bb3f1dc96
gvalves <gva@geekcode.com.br>
2020-12-14 14:49:01 -0300

    Finished graphql api

---
+++ graphql/video/VideoResolver.ts
@@ -0,0 +1,28 @@
+import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+import VideoSchema from "../../model/VideoSchema";
+import Video from "./Video";
+
+@InputType()
+class VideoInput {
+  @Field()
+  name: String;
+  @Field()
+  description: String;
+  _id: String;
+}
+
+@Resolver(Video)
+class VideoResolver {
+  @Query(() => [Video])
+  async videos() {
+    const videos = await VideoSchema.find();
+    return videos;
+  }
+  @Mutation(() => Video)
+  async createVideo(@Arg("videoInput") videoInput: VideoInput) {
+    const video = await VideoSchema.create(videoInput);
+    return video;
+  }
+}
+
+export default VideoResolver;
